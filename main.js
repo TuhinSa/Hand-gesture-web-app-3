2e8e3c8cbdb16fa52736fbb0508b5b709c344bb3
TuhinSa <92089255+TuhinSa@users.noreply.github.com>
2022-04-09 07:15:10 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+Webcam.set ({
+    width: 300,
+    height: 300,
+    image_format : 'png',
+    png_quality : 90
+
+});
+camera = document.getElementById("camera")
+Webcam.attach("#camera")
+function takesnapshot()
+{
+    Webcam.snap(function(data_url) {
+        document.getElementById("result").innerHTML = '<img id = "final_picture" src = " ' + data_url + '"/>'
+    } )
+}
+console.log("ml5 version:", ml5.version);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/0RMLP8C9i/model.json', modelLoaded)
+function modelLoaded(){
+    console.log("Model Loaded")
+}
+function check() {
+    img = document.getElementById("final_picture")
+    classifier.classify(img, gotResult)
+}
+function gotResult(error, results) 
+{
+    if(error) {
+        console.error("error")
+    }
+    if(results){
+        document.getElementById("result_gesture").innerHTML = results[0].label;
+    }
+}
